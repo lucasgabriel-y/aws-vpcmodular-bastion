ae0d77848e8ef0b12a666007c71f6214bc5dbf78
Lucas Gabriel <gabriel.lglg93@gmail.com>
2023-07-12 11:49:11 -0300

    Add files via upload

--- vpcbastion.tf
+++ vpcbastion.tf
@@ -46,23 +46,15 @@ resource "aws_security_group" "public_security_group_bastion" {
 
 resource "aws_security_group" "bastion_sg" {
   name        = "bastion_sg"
-  description = "Acesso"
+  description = "Acesso SSH"
   vpc_id      = aws_vpc.bastion_vpc.id
 
   ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
 
 
   tags = {
